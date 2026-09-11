11764359ddde6a123ca07410a0f55fa0f8a605a3
Harrick Christo JP <jpharrickchristo@gmail.com>
2026-09-11 21:58:05 +0530

    Time: 2 ms (69.41%), Space: 45 MB (12.07%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans=new ArrayList<>();
         if(n<1) return ans;
@@ -8,13 +8,30 @@ class Solution {
 
     }
     public static void call(int open,int close,String str,List<String> ans){
-        if(open ==0 && close==0){
+        if(open ==0 && close==0){ //when we open & closed all the paranthesis
+            ans.add(str);// which the above is the base case
+            return;//
+        }
+        if(open>0) call(open-1,close,str+"(",ans); // we should always start with  the open brackets 
+
+        if(close>open) call(open,close-1,str+")",ans);
+        //closing the paranthesis
+    }
+}*/
+
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        ArrayList<String> ans =new ArrayList<>();
+        call(ans,n,n,"");
+        return ans;
+    }
+    public static void call(ArrayList<String> ans,int open,int close,String str){
+        if(open==0 && close==0){
             ans.add(str);
             return;
         }
-        if(open>0) call(open-1,close,str+"(",ans);
-
-        if(close>open) call(open,close-1,str+")",ans);
-
+        if(open>0) call(ans,open-1,close,str+"(");
+        if(close>open) call(ans,open,close-1,str+")");
     }
 }
